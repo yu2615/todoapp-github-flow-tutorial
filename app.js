0319883a51b0013c6c075dcde154d8701349b75c
81801 <19AJ088@ms.dendai.ac.jp>
2023-11-09 18:55:13 +0900

    完了未完了の実装が完了しました

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ $(document).on("change", "input[type=checkbox]", function () {
     $(this).parent().css("color", "#ccc");
   } else {
     $(this).parent().css("text-decoration", "none");
-    $(this).parent().css("color", "#000");
+    $(this).parent().css("color", "#001");
   }
 });
